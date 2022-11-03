07a59e6e094bd4cc8526d1706bcac4051e90090f
bgebes <berkay.gebes@bil.omu.edu.tr>
2022-11-04 02:05:28 +0300

    feat: invert numbers and added positions to mock data

--- src/components/Area/mock.js
+++ src/components/Area/mock.js
@@ -1,322 +1,514 @@
 export const mock = [
   {
     bgColor: 'white',
-    position: {},
+    position: {
+      x: 'a',
+      y: '8',
+    },
     checker: null,
   },
   {
     bgColor: 'gray.400',
-    position: {},
+    position: {
+      x: 'b',
+      y: '8',
+    },
     checker: null,
   },
   {
     bgColor: 'white',
-    position: {},
+    position: {
+      x: 'c',
+      y: '8',
+    },
     checker: null,
   },
   {
     bgColor: 'gray.400',
-    position: {},
+    position: {
+      x: 'd',
+      y: '8',
+    },
     checker: null,
   },
   {
     bgColor: 'white',
-    position: {},
+    position: {
+      x: 'e',
+      y: '8',
+    },
     checker: null,
   },
   {
     bgColor: 'gray.400',
-    position: {},
+    position: {
+      x: 'f',
+      y: '8',
+    },
     checker: null,
   },
   {
     bgColor: 'white',
-    position: {},
+    position: {
+      x: 'g',
+      y: '8',
+    },
     checker: null,
   },
   {
     bgColor: 'gray.400',
-    position: {},
+    position: {
+      x: 'h',
+      y: '8',
+    },
     checker: null,
   },
   {
     bgColor: 'gray.400',
-    position: {},
+    position: {
+      x: 'a',
+      y: '7',
+    },
+    checker: 'red',
+  },
+  {
+    bgColor: 'white',
+    position: {
+      x: 'b',
+      y: '7',
+    },
+    checker: 'red',
+  },
+  {
+    bgColor: 'gray.400',
+    position: {
+      x: 'c',
+      y: '7',
+    },
+    checker: 'red',
+  },
+  {
+    bgColor: 'white',
+    position: {
+      x: 'd',
+      y: '7',
+    },
+    checker: 'red',
+  },
+  {
+    bgColor: 'gray.400',
+    position: {
+      x: 'e',
+      y: '7',
+    },
+    checker: 'red',
+  },
+  {
+    bgColor: 'white',
+    position: {
+      x: 'f',
+      y: '7',
+    },
+    checker: 'red',
+  },
+  {
+    bgColor: 'gray.400',
+    position: {
+      x: 'g',
+      y: '7',
+    },
+    checker: 'red',
+  },
+  {
+    bgColor: 'white',
+    position: {
+      x: 'h',
+      y: '7',
+    },
+    checker: 'red',
+  },
+  {
+    bgColor: 'white',
+    position: {
+      x: 'a',
+      y: '6',
+    },
+    checker: 'red',
+  },
+  {
+    bgColor: 'gray.400',
+    position: {
+      x: 'b',
+      y: '6',
+    },
+    checker: 'red',
+  },
+  {
+    bgColor: 'white',
+    position: {
+      x: 'c',
+      y: '6',
+    },
+    checker: 'red',
+  },
+  {
+    bgColor: 'gray.400',
+    position: {
+      x: 'd',
+      y: '6',
+    },
+    checker: 'red',
+  },
+  {
+    bgColor: 'white',
+    position: {
+      x: 'e',
+      y: '6',
+    },
+    checker: 'red',
+  },
+  {
+    bgColor: 'gray.400',
+    position: {
+      x: 'f',
+      y: '6',
+    },
+    checker: 'red',
+  },
+  {
+    bgColor: 'white',
+    position: {
+      x: 'g',
+      y: '6',
+    },
+    checker: 'red',
+  },
+  {
+    bgColor: 'gray.400',
+    position: {
+      x: 'h',
+      y: '6',
+    },
+    checker: 'red',
+  },
+  {
+    bgColor: 'gray.400',
+    position: {
+      x: 'a',
+      y: '5',
+    },
     checker: null,
   },
   {
     bgColor: 'white',
-    position: {},
+    position: {
+      x: 'b',
+      y: '5',
+    },
     checker: null,
   },
   {
     bgColor: 'gray.400',
-    position: {},
+    position: {
+      x: 'c',
+      y: '5',
+    },
     checker: null,
   },
   {
     bgColor: 'white',
-    position: {},
+    position: {
+      x: 'd',
+      y: '5',
+    },
     checker: null,
   },
   {
     bgColor: 'gray.400',
-    position: {},
+    position: {
+      x: 'e',
+      y: '5',
+    },
     checker: null,
   },
   {
     bgColor: 'white',
-    position: {},
+    position: {
+      x: 'f',
+      y: '5',
+    },
     checker: null,
   },
   {
     bgColor: 'gray.400',
-    position: {},
+    position: {
+      x: 'g',
+      y: '5',
+    },
     checker: null,
   },
   {
     bgColor: 'white',
-    position: {},
+    position: {
+      x: 'h',
+      y: '5',
+    },
     checker: null,
   },
   {
     bgColor: 'white',
-    position: {},
+    position: {
+      x: 'a',
+      y: '4',
+    },
     checker: null,
   },
   {
     bgColor: 'gray.400',
-    position: {},
+    position: {
+      x: 'b',
+      y: '4',
+    },
     checker: null,
   },
   {
     bgColor: 'white',
-    position: {},
+    position: {
+      x: 'c',
+      y: '4',
+    },
     checker: null,
   },
   {
     bgColor: 'gray.400',
-    position: {},
+    position: {
+      x: 'd',
+      y: '4',
+    },
     checker: null,
   },
   {
     bgColor: 'white',
-    position: {},
+    position: {
+      x: 'e',
+      y: '4',
+    },
     checker: null,
   },
   {
     bgColor: 'gray.400',
-    position: {},
+    position: {
+      x: 'f',
+      y: '4',
+    },
     checker: null,
   },
   {
     bgColor: 'white',
-    position: {},
+    position: {
+      x: 'g',
+      y: '4',
+    },
     checker: null,
   },
   {
     bgColor: 'gray.400',
-    position: {},
+    position: {
+      x: 'h',
+      y: '4',
+    },
     checker: null,
   },
   {
     bgColor: 'gray.400',
-    position: {},
+    position: {
+      x: 'a',
+      y: '3',
+    },
+    checker: 'blue',
+  },
+  {
+    bgColor: 'white',
+    position: {
+      x: 'b',
+      y: '3',
+    },
+    checker: 'blue',
+  },
+  {
+    bgColor: 'gray.400',
+    position: {
+      x: 'c',
+      y: '3',
+    },
+    checker: 'blue',
+  },
+  {
+    bgColor: 'white',
+    position: {
+      x: 'd',
+      y: '3',
+    },
+    checker: 'blue',
+  },
+  {
+    bgColor: 'gray.400',
+    position: {
+      x: 'e',
+      y: '3',
+    },
+    checker: 'blue',
+  },
+  {
+    bgColor: 'white',
+    position: {
+      x: 'f',
+      y: '3',
+    },
+    checker: 'blue',
+  },
+  {
+    bgColor: 'gray.400',
+    position: {
+      x: 'g',
+      y: '3',
+    },
+    checker: 'blue',
+  },
+  {
+    bgColor: 'white',
+    position: {
+      x: 'h',
+      y: '3',
+    },
+    checker: 'blue',
+  },
+  {
+    bgColor: 'white',
+    position: {
+      x: 'a',
+      y: '2',
+    },
+    checker: 'blue',
+  },
+  {
+    bgColor: 'gray.400',
+    position: {
+      x: 'b',
+      y: '2',
+    },
+    checker: 'blue',
+  },
+  {
+    bgColor: 'white',
+    position: {
+      x: 'c',
+      y: '2',
+    },
+    checker: 'blue',
+  },
+  {
+    bgColor: 'gray.400',
+    position: {
+      x: 'd',
+      y: '2',
+    },
+    checker: 'blue',
+  },
+  {
+    bgColor: 'white',
+    position: {
+      x: 'e',
+      y: '2',
+    },
+    checker: 'blue',
+  },
+  {
+    bgColor: 'gray.400',
+    position: {
+      x: 'f',
+      y: '2',
+    },
+    checker: 'blue',
+  },
+  {
+    bgColor: 'white',
+    position: {
+      x: 'g',
+      y: '2',
+    },
+    checker: 'blue',
+  },
+  {
+    bgColor: 'gray.400',
+    position: {
+      x: 'h',
+      y: '2',
+    },
+    checker: 'blue',
+  },
+  {
+    bgColor: 'gray.400',
+    position: {
+      x: 'a',
+      y: '1',
+    },
     checker: null,
   },
   {
     bgColor: 'white',
-    position: {},
+    position: {
+      x: 'b',
+      y: '1',
+    },
     checker: null,
   },
   {
     bgColor: 'gray.400',
-    position: {},
+    position: {
+      x: 'c',
+      y: '1',
+    },
     checker: null,
   },
   {
     bgColor: 'white',
-    position: {},
+    position: {
+      x: 'd',
+      y: '1',
+    },
     checker: null,
   },
   {
     bgColor: 'gray.400',
-    position: {},
+    position: {
+      x: 'e',
+      y: '1',
+    },
     checker: null,
   },
   {
     bgColor: 'white',
-    position: {},
+    position: {
+      x: 'f',
+      y: '1',
+    },
     checker: null,
   },
   {
     bgColor: 'gray.400',
-    position: {},
+    position: {
+      x: 'g',
+      y: '1',
+    },
     checker: null,
   },
   {
     bgColor: 'white',
-    position: {},
-    checker: null,
-  },
-  {
-    bgColor: 'white',
-    position: {},
-    checker: null,
-  },
-  {
-    bgColor: 'gray.400',
-    position: {},
-    checker: null,
-  },
-  {
-    bgColor: 'white',
-    position: {},
-    checker: null,
-  },
-  {
-    bgColor: 'gray.400',
-    position: {},
-    checker: null,
-  },
-  {
-    bgColor: 'white',
-    position: {},
-    checker: null,
-  },
-  {
-    bgColor: 'gray.400',
-    position: {},
-    checker: null,
-  },
-  {
-    bgColor: 'white',
-    position: {},
-    checker: null,
-  },
-  {
-    bgColor: 'gray.400',
-    position: {},
-    checker: null,
-  },
-  {
-    bgColor: 'gray.400',
-    position: {},
-    checker: null,
-  },
-  {
-    bgColor: 'white',
-    position: {},
-    checker: null,
-  },
-  {
-    bgColor: 'gray.400',
-    position: {},
-    checker: null,
-  },
-  {
-    bgColor: 'white',
-    position: {},
-    checker: null,
-  },
-  {
-    bgColor: 'gray.400',
-    position: {},
-    checker: null,
-  },
-  {
-    bgColor: 'white',
-    position: {},
-    checker: null,
-  },
-  {
-    bgColor: 'gray.400',
-    position: {},
-    checker: null,
-  },
-  {
-    bgColor: 'white',
-    position: {},
-    checker: null,
-  },
-  {
-    bgColor: 'white',
-    position: {},
-    checker: null,
-  },
-  {
-    bgColor: 'gray.400',
-    position: {},
-    checker: null,
-  },
-  {
-    bgColor: 'white',
-    position: {},
-    checker: null,
-  },
-  {
-    bgColor: 'gray.400',
-    position: {},
-    checker: null,
-  },
-  {
-    bgColor: 'white',
-    position: {},
-    checker: null,
-  },
-  {
-    bgColor: 'gray.400',
-    position: {},
-    checker: null,
-  },
-  {
-    bgColor: 'white',
-    position: {},
-    checker: null,
-  },
-  {
-    bgColor: 'gray.400',
-    position: {},
-    checker: null,
-  },
-  {
-    bgColor: 'gray.400',
-    position: {},
-    checker: null,
-  },
-  {
-    bgColor: 'white',
-    position: {},
-    checker: null,
-  },
-  {
-    bgColor: 'gray.400',
-    position: {},
-    checker: null,
-  },
-  {
-    bgColor: 'white',
-    position: {},
-    checker: null,
-  },
-  {
-    bgColor: 'gray.400',
-    position: {},
-    checker: null,
-  },
-  {
-    bgColor: 'white',
-    position: {},
-    checker: null,
-  },
-  {
-    bgColor: 'gray.400',
-    position: {},
-    checker: null,
-  },
-  {
-    bgColor: 'white',
-    position: {},
+    position: {
+      x: 'h',
+      y: '1',
+    },
     checker: null,
   },
 ];
--- src/components/Numbers/Numbers.jsx
+++ src/components/Numbers/Numbers.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Stack, Text } from '@chakra-ui/react';
 
 function Numbers() {
-  const numbersArr = [1, 2, 3, 4, 5, 6, 7, 8];
+  const numbersArr = [8, 7, 6, 5, 4, 3, 2, 1];
 
   return (
     <Stack justify="space-evenly" me="5" pt="3">
